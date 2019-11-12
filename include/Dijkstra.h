896d6f8729e7cd560b682557a1bb18d742d30c5e
Louis Ledru <louis.ledru@utbm.fr>
2019-11-12 18:39:08 +0100

    dijkstra

---
+++ include/Dijkstra.h
@@ -0,0 +1,18 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+
+class Dijkstra
+{
+    public:
+        Dijkstra();
+        virtual ~Dijkstra();
+        Dijkstra(const Dijkstra& other);
+        Dijkstra& operator=(const Dijkstra& other);
+
+    protected:
+
+    private:
+};
+
+#endif // DIJKSTRA_H
---
+++ src/Dijkstra.cpp
@@ -0,0 +1,23 @@
+#include "Dijkstra.h"
+
+Dijkstra::Dijkstra()
+{
+    //ctor
+}
+
+Dijkstra::~Dijkstra()
+{
+    //dtor
+}
+
+Dijkstra::Dijkstra(const Dijkstra& other)
+{
+    //copy ctor
+}
+
+Dijkstra& Dijkstra::operator=(const Dijkstra& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
